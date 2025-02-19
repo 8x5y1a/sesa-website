c899df3b5ac67313f74c494e3e81e093d794b3c1
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-02-19 15:50:24 -0500

    feat: Add navbar
    Not yet complete and probably not the most Tailwind-y way to do things
    wrt fonts and whatnot, but that can be updated later.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+
 export default function Navbar() {
-  return (
-    <div>
-      <p>This is the navbar!</p>
-    </div>
-  );
+    return (
+        <header className="flex justify-between my-8 mx-16 [backdrop-filter:blur(100px)] bg-gray">
+            <div className="flex gap-16">
+                <Image className="my-0" width="50" height="50" src="sesa-logo.svg" alt="SESA Logo" />
+                <div className="flex gap-16 items-center">
+                    <a className="uppercase text-xl mono my-4" href="#">Team</a>
+                    <a className="uppercase text-xl mono my-4" href="#">Events</a>
+                    <a className="uppercase text-xl mono my-4" href="#">Resources</a>
+                    <a className="uppercase text-xl mono my-4" href="#">Contact</a>
+                </div>
+            </div>
+            <div className="flex gap-8 items-center">
+                <a role="button" className="uppercase px-6 py-2 text-xl mono stroke-gradient clip-corner" href="#">Discord</a>
+                <a role="button" className="uppercase px-6 py-2 text-xl mono fill-gradient clip-corner" href="#">Sponsor</a>
+            </div>
+        </header>
+    );
 }
