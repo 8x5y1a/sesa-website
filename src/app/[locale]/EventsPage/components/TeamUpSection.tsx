8x5y1a/sesa-website

commit 1618a3ea32d134eca8f6faf80100ce1c40bffa06

sponsor page initial commit (#64)
* sponsor page initial commit

* Update resourcesData.tsx

* sponsor img sizes fixed (not responsive yet)

* sponsor page changes

* color fixes

* btn link

* linting issues resolved

---------

Co-authored-by: Asad Ali <masadbali190@gmail.com>

src/app/[locale]/SponsorsPage/components/CTA.tsx
@@ -1,0 +1,24 @@
+"use client";
+import Button from "@/components/Button";
+import Link from "next/link";
+
+export default function CTA() {
+    return (
+        <div className="relative z-10 flex w-full flex-col items-center justify-center px-4 py-8 text-center">
+            <div className="flex flex-wrap items-center justify-center gap-3">
+                <p className="text-m font-sans text-white">
+                    Interested in joining our growing network of industry partners?
+                </p>
+                <Link href="/contact">
+                    <Button className="px-4 py-1.5 font-heading text-sm uppercase text-white">
+                        Let&apos;s Talk
+                    </Button>
+                </Link>
+            </div>
+
+            <p className="mt-4 font-sans text-sm text-thistle">
+                We&apos;ll get back to you within 48 hours.
+            </p>
+        </div>
+    );
+}

src/app/[locale]/SponsorsPage/components/SponsorCard.tsx
@@ -1,0 +1,80 @@
+"use client";
+
+import Image from "next/image";
+import Button from "@/components/Button";
+import CircleImage from "@/components/CircleImage";
+import IconButton from "@/components/IconButton";
+
+const SponsorCard = () => {
+    return (
+        <div className="my-36 flex h-[45rem] items-center justify-start gap-12 align-middle">
+            <div className="grid-overlay-left md:h-[43.93rem] md:w-[53vw]"></div>
+
+            <div className="relative z-10 lg:block">
+                <Image
+                    src="/sponsors-page/sponsor-card-img.png"
+                    alt="SESA Group Photo"
+                    className="h-[500px] w-auto"
+                    width={700}
+                    height={700}
+                />
+            </div>
+
+            <div className="z-10 max-w-lg text-left text-white">
+                {/* Warp logo */}
+                <Image
+                    src="/sponsors/warp.png"
+                    alt="Warp logo"
+                    width={70}
+                    height={20}
+                    className="mb-4"
+                />
+                <div className="mt-3 font-heading text-2xl uppercase text-white">
+                    &ldquo;PARTNERING WITH SESA HAS BEEN AN{" "}
+                    <span className="relative inline-block">
+                        INCREDIBLE EXPERIENCE.
+                        <div className="absolute right-0 top-0 h-full w-0 animate-highlight [background:linear-gradient(55.37deg,_rgba(136,_36,_220,_0.25),_rgba(177,_33,_97,_0.25))]"></div>
+                    </span>{" "}
+                    THE VISIBILITY AND CONNECTIONS WE&apos;VE GAINED ARE UNMATCHED.&rdquo;
+                </div>
+
+                <div className="mt-4 flex items-center gap-3">
+                    <CircleImage size={50} src="/sponsors-page/asad.png" alt="Asad" />
+                    <div>
+                        <p className="text-m font-bold text-white">Jack Sparrow</p>
+                        <p className="text-sm text-thistle">Product Growth at Warp</p>
+                    </div>
+                </div>
+                <div className="mt-6">
+                    <Button
+                        href="/pages/ContactUsPage"
+                        className="text-m font-heading uppercase text-white"
+                    >
+                        Work With Us
+                    </Button>
+                </div>
+            </div>
+            {/* Navigation Buttons */}
+            <div className="flex items-center gap-2">
+                <IconButton variant="outline">
+                    <Image
+                        src="/resources-page/arrow_backword.svg"
+                        width={25}
+                        height={25}
+                        alt="Left"
+                    />
+                </IconButton>
+                <IconButton variant="outline">
+                    <Image
+                        src="/resources-page/arrow_forward.svg"
+                        width={25}
+                        height={25}
+                        alt="Right"
+                    />
+                </IconButton>
+            </div>
+        </div>
+    );
+};
+
+export default SponsorCard;

src/app/[locale]/SponsorsPage/page.tsx
@@ -1,0 +1,28 @@
+"use client";
+import Header from "../SponsorsPage/components/Header";
+import Benefits from "./components/Benefits";
+import CTA from "./components/CTA";
+import PastCollaboratorsCarousel from "./components/PastCollaboratorsCarousel";
+import SponsorCard from "./components/SponsorCard";
+import SponsorsGrid from "./components/SponsorsGrid";
+
+const SponsorsPage = () => {
+    return (
+        <>
+            <Header
+                topText="Sponsor & partners"
+                title="HUGE THANKS TO OUR GENEROUS SPONSORS AND PARTNERS"
+                bottomText="Our incredible partnerships make SESA possible at uOttawa. Thank you for believing in our mission!"
+                btn1="Become a sponsor"
+                btn2="View Benefits"
+            />
+            <SponsorsGrid />
+            <CTA />
+            <SponsorCard />
+            <PastCollaboratorsCarousel />
+            <Benefits />
+        </>
+    );
+};
+
+export default SponsorsPage;

commit 734b7138ba12d11726061f716f4b92e9828950ee

Fix broken internal links (#133)
* fix: Fix broken internal links

The `pages/` folder hasn't existed for a while.

* fix(sponsor): Fix sponsorship page contact link

Fixes: 1618a3e (sponsor page initial commit (#64))

src/app/[locale]/EventsPage/components/TeamUpSection.tsx
@@ -33,7 +33,7 @@ const TeamUpSection: React.FC = () => {
 
                 <div className="mt-6 flex flex-wrap gap-4">
                     <Button className="relative z-10 font-heading text-xl uppercase" asChild>
-                        <Link href="/pages/ContactUsPage">{t("btn_talk")}</Link>
+                        <Link href="/ContactUsPage">{t("btn_talk")}</Link>
                     </Button>
 
                     <Button
@@ -41,7 +41,7 @@ const TeamUpSection: React.FC = () => {
                         className="relative z-10 font-heading text-xl uppercase"
                         asChild
                     >
-                        <Link href="/pages/TeamPage">{t("btn_meet_team")}</Link>
+                        <Link href="/TeamPage">{t("btn_meet_team")}</Link>
                     </Button>
                 </div>
 

src/app/[locale]/ResourcesPage/components/FooterSection.tsx
@@ -83,7 +83,7 @@ const FooterSection: React.FC = () => {
                     {/* Call To Action Button */}
                     <div className="mt-10 md:mt-6">
                         <Button className="relative z-10 font-heading text-lg uppercase" asChild>
-                            <Link href="/pages/TeamPage">{t("btn_join_team")}</Link>
+                            <Link href="/TeamPage">{t("btn_join_team")}</Link>
                         </Button>
                         <div className="mt-6 flex items-center gap-2">
                             <Image

src/app/[locale]/SponsorsPage/components/CTA.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 
 export default function CTA() {
     return (
@@ -9,7 +9,7 @@ export default function CTA() {
                 <p className="font-heading text-lg uppercase text-white">
                     Interested in joining our growing network of industry partners?
                 </p>
-                <Link href="/contact">
+                <Link href="/ContactUsPage">
                     <Button className="px-4 py-1.5 font-heading text-sm uppercase text-white">
                         Let&apos;s Talk
                     </Button>

src/app/[locale]/SponsorsPage/components/SponsorCard.tsx
@@ -46,7 +46,7 @@ const SponsorCard = () => {
                 </div>
                 <div className="mt-6">
                     <Button className="text-m font-heading uppercase text-white" asChild>
-                        <Link href="/pages/ContactUsPage">Work With Us</Link>
+                        <Link href="/ContactUsPage">Work With Us</Link>
                     </Button>
                 </div>
             </div>

src/app/[locale]/SponsorsPage/components/SponsorCard2.tsx
@@ -38,10 +38,10 @@ const SponsorCard2 = () => {
 
                     <div className="mb-6 flex items-center gap-4">
                         <Button className="font-heading uppercase text-white" asChild>
-                            <Link href="/pages/ContactUsPage">Become a Sponsor</Link>
+                            <Link href="/ContactUsPage">Become a Sponsor</Link>
                         </Button>
                         <Button variant="outline" className="font-heading text-white">
-                            <Link href="/pages/AboutPage">Meet the Team</Link>
+                            <Link href="/AboutPage">Meet the Team</Link>
                         </Button>
                     </div>
 

src/app/[locale]/resources/[id]/page.tsx
@@ -76,7 +76,7 @@ export default async function ResourcePage({ params }: { params: Promise<{ id: s
                     className="absolute left-72 top-0 font-heading uppercase"
                     asChild
                 >
-                    <Link href="/pages/ResourcesPage">Go Back</Link>
+                    <Link href="/ResourcesPage">Go Back</Link>
                 </Button>
                 <div className="flex min-w-[65ch] flex-col gap-32">
                     {/* Resource */}